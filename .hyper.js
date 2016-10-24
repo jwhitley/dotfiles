0901b2e27cf1dde28ed2eb602c74a0a2a05cdf45
John Whitley <john@luminous-studios.com>
2016-10-24 14:54:06 -0700

    Add IR Black theme plugin

--- .hyper.js
+++ .hyper.js
@@ -82,7 +82,9 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'hyperterm-ir-black',
+  ],
 
   // in development, you can create a directory under
   // `~/.hyperterm_plugins/local/` and include it here
